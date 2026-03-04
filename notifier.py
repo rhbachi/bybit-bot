a725da23dcb1c51e02b4ca7cf95670569264f0f3
rhbachi <mohammed.rhbachi@gmail.com>
2026-03-04 23:15:53 +0100

    telegram debug version

--- notifier.py
+++ notifier.py
@@ -4,11 +4,13 @@ import requests
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
 
+print("📨 Telegram BOT TOKEN:", BOT_TOKEN)
+print("📨 Telegram CHAT ID:", CHAT_ID)
 
 def send_telegram(message):
 
     if not BOT_TOKEN or not CHAT_ID:
-        print("⚠️ Telegram non configuré")
+        print("❌ Telegram variables manquantes")
         return
 
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
@@ -21,11 +23,13 @@ def send_telegram(message):
 
     try:
 
-        r = requests.post(url, json=payload, timeout=5)
+        r = requests.post(url, json=payload, timeout=10)
+
+        print("📩 Telegram status:", r.status_code)
 
         if r.status_code != 200:
-            print("Telegram error:", r.text)
+            print("❌ Telegram error:", r.text)
 
     except Exception as e:
 
-        print("Telegram exception:", e)
+        print("❌ Telegram exception:", e)
